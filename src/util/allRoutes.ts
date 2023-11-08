cd13717546cfc75f320328638a386ea0da42771f
alfiya <alfiya96960@gmail.com>
2023-11-08 16:45:30 +0530

    minor-changes

--- src/util/allRoutes.ts
+++ src/util/allRoutes.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import userRoutes from "../routes/userRoutes";
 import productRoutes from "../routes/productRoutes";
 import mediaRoutes from "../routes/mediaRotes";
+import orderRoutes from "../routes/orderRoutes";
+import cartRoutes from "../routes/cartroutes";
 const router = Router();
 
 router.use("/user", userRoutes);
 router.use("/product", productRoutes);
 router.use("/media", mediaRoutes);
+router.use("/order", orderRoutes);
+router.use("/cart", cartRoutes);
 
 export default router;
