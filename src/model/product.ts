4636bd66eeb0c15adf8b06df7c7b359dfd8bfa7d
alfiya <alfiya96960@gmail.com>
2023-11-08 16:49:09 +0530

    minor-changes

--- src/model/product.ts
+++ src/model/product.ts
@@ -2,58 +2,65 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IProduct extends Document {
-  name: string;
-  category: string;
-  size: string[];
-  price: number;
-  media: object;
-  sales_price: number;
-  details: object;
-  description: string;
+    name: string;
+    category: string;
+    size: string[];
+    price: number;
+    media: object;
+    sales_price: number;
+    details: object;
+    description: string;
+    inStock: string;
 }
 
 const productSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    size: {
-      type: [String],
-      enum: ["XXL", "XL", "L", "M", "XXXS", "XXS", "XS", "S"],
-      default: ["XXL", "XL", "L", "M", "XXXS", "XXS", "XS", "S"],
-    },
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: [String],
+            enum: ["XXL", "XL", "L", "M", "XXXS", "XXS", "XS", "S"],
+            default: ["XXL", "XL", "L", "M", "XXXS", "XXS", "XS", "S"],
+        },
 
-    price: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    sales_price: {
-      type: Number,
-      default: 0,
-    },
-    media: [
-      {
-        type: {
-          type: String,
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
         },
-        url: {
-          type: String,
+        sales_price: {
+            type: Number,
+            default: 0,
         },
-      },
-    ],
-    details: {},
-    description: {
-      type: String,
-      required: true,
+        media: [
+            {
+                type: {
+                    type: String,
+                },
+                url: {
+                    type: String,
+                },
+            },
+        ],
+        inStock: {
+            type: String,
+            required: true,
+            default: "Unavailbale",
+            enum: ["Available", "Unavailable"],
+        },
+        details: {},
+        description: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model<IProduct>("Product", productSchema);
