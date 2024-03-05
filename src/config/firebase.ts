0d6167ed04ce7ddb10658fb8bcc8d8729b15a91b
alfiya <alfiya96960@gmail.com>
2024-03-05 12:54:35 +0530

    minor changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,13 +7,13 @@ const {
 } = require("firebase/storage");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzVO4Qdqpew5C4yHPfD0y6tPgNby36dtU",
-    authDomain: "ecommerce-a7061.firebaseapp.com",
-    projectId: "ecommerce-a7061",
-    storageBucket: "ecommerce-a7061.appspot.com",
-    messagingSenderId: "473417194276",
-    appId: "1:473417194276:web:748cc909500d0fec19b1b1",
-    measurementId: "G-K83SYTXLP7",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 // Initialize Firebase
